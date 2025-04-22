6ab38031874b260b796f92fbed21d3e639cf718a
Josue Maldonado <Memejosue2012@hotmail.com>
2025-04-22 07:32:59 -0600

    Agregado Log Out

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Sidebar.css';
 
-const Sidebar = () => (
-  <aside className="sidebar">
-    <h2>Menú</h2>
-    <nav>
-      <button>Dashboard</button>
-      <button>Películas</button>
-      <button>Reservas</button>
-      <button>Perfil</button>
-    </nav>
-  </aside>
-);
+const Sidebar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('token'); // Quitar token
+    navigate('/'); // Redirigir a login
+  };
+
+  return (
+    <aside className="sidebar">
+      <h2>Menú</h2>
+      <nav>
+        <button>Dashboard</button>
+        <button>Películas</button>
+        <button>Reservas</button>
+        <button>Perfil</button>
+        <button onClick={handleLogout}>Cerrar sesión</button>
+      </nav>
+    </aside>
+  );
+};
 
 export default Sidebar;
